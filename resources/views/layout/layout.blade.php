be1c0a4f536660675e8f7102e12f344b790f1662
Mhyth <leanderjose7@gmail.com>
2021-12-21 13:29:30 +0700

    fix header in some pages

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/header.css">
+    {{-- <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/footer.css">
-    <link rel="stylesheet" href="css/createGame.css">
+    <link rel="stylesheet" href="css/createGame.css"> --}}
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('css/footer.css') }}" rel="stylesheet" type="text/css" >
+    <link href="{{ asset('css/createGame.css') }}" rel="stylesheet" type="text/css" >
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
