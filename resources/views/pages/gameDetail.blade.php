fcc9994c92a64c68cf62fb364b1abc0e671332db
Mhyth <leanderjose7@gmail.com>
2021-12-21 19:42:43 +0700

    modify game detail

--- resources/views/pages/gameDetail.blade.php
+++ resources/views/pages/gameDetail.blade.php
@@ -1,13 +1,43 @@
 @extends('layout.layout')
 @section('content')
 <main>
-    <h2>{{$game->name}}</h2>
-    <img src={{Storage::url($game->cover)}} alt={{$game->name}}>
-    <p>{{$game->description}}</p>
-    <p>Genre: {{$game->category}}</p>
-    <p>Release Date: {{$game->release_date}}</p>
-    <p>Developer: {{$game->developer}}</p>
-    <p>Publisher: {{$game->publisher}}</p>
+    <div id="game-detail-container">
+        <div id="bread-container">
+            <div id="bread">
+                <ol style="--bs-breadcrumb-divider: '>';" class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#"><img src="/images/home.png" alt=""></a></li>
+                    <li class="breadcrumb-item">{{$game->category}}</li>
+                    <li class="breadcrumb-item active" aria-current="page">{{$game->name}}</li>
+                </ol>
+            </div>
+        </div>
+        <div id="detail-game">
+            <div id="video-container">
+                <video width='900px' height="506px" controls>
+                <source src={{Storage::url($game->trailer)}} type="video/webm">
+                </video>
+            </div>
+            <div id="detail-game-container">
+                <div id="game-img-container"><img src={{Storage::url($game->cover)}} alt={{$game->name}}></div>
+                <div id="game-title-container"><h2>{{$game->name}}</h2></div>
+                <div><p>{{$game->description}}</p></div>
+                <div id="game-de-container"><p>Genre:</p>{{$game->category}}</div>
+                <div id="game-de-container"><p>Release Date: </p>{{$game->release_date}}</div>
+                <div id="game-de-container"><p>Developer: </p>{{$game->developer}}</div>
+                <div id="game-de-container"><p>Publisher: </p>{{$game->publisher}}</div>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+    
+    
+    
+    
+   
+    
+    
     @guest
     <a href="/login">Rp. {{$game->price}} | Add to cart</a>
     @endguest
@@ -16,9 +46,6 @@
             <a href='/cart/{{$game->game_id}}'>Rp. {{$game->price}} | Add to cart</a>
         @endif   
     @endauth
-    <video width='600px' height="400px">
-        <source src={{Storage::url($game->trailer)}} type="video/webm">
-    </video>
     <p>ABOUT THIS GAME</p>
     <p>{{$game->description_long}}</p>
     @if ($errors->any())
