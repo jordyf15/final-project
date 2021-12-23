fd8f4fcf34d803abfa5c02ea03aa793383b6a0d6
jordyf15 <jordyferdian88@gmail.com>
2021-12-23 21:18:23 +0700

    styled receipt page

--- resources/views/pages/transactionReceipt.blade.php
+++ resources/views/pages/transactionReceipt.blade.php
@@ -1,25 +1,31 @@
 @extends('layout.layout')
 @section('content')
-<main>
-    <div>
-        <p>Shopping Cart</p>
-        <p>Transaction Information</p>
-        <p>Transaction Receipt</p>
+<main id="receipt-main">
+    <div id="bread-container">
+        <div id="bread">
+            <ol style="--bs-breadcrumb-divider: '>';" class="breadcrumb">
+                <li class="breadcrumb-item active">Shopping Cart</li>
+                <li class="breadcrumb-item">Transaction Information</li>
+                <li class="breadcrumb-item">Transaction Receipt</li>
+            </ol>
+        </div>
     </div>
     <h1>Transaction Receipt</h1>
-    <div>
-        <p>Transaction ID: {{$transactionHeader->transaction_header_id}}</p>
+    <div id='receipt-container'>
+        <p id='receipt-id'>Transaction ID: {{$transactionHeader->transaction_header_id}}</p>
         <p>Purchased Date: {{$transactionHeader->purchase_date}}</p>
         <div>
             @for($i = 0; $i<count($transactionHeader->transactionDetails);$i++)
-            <div>
+            <div class='receipt-items'>
                 <img src={{Storage::url($transactionHeader->transactionDetails[$i]->game->cover)}} alt="">
-                <p>{{$transactionHeader->transactionDetails[$i]->game->name}}</p>
-                <p>Rp. {{$transactionHeader->transactionDetails[$i]->game->price}}</p>
+                <div class='receipt-items-texts'>
+                    <p class='receipt-items-names'>{{$transactionHeader->transactionDetails[$i]->game->name}}</p>
+                    <p>Rp. {{$transactionHeader->transactionDetails[$i]->game->price}}</p>
+                </div>
             </div>
             @endfor
         </div>
-        <p>Total Price Rp. {{$transactionHeader->total_price}}</p>
+        <p>Total Price <span id='receipt-total'>Rp. {{$transactionHeader->total_price}}</span></p>
     </div>
 </main>
 @endsection
