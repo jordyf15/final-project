85213e1ec53e2297853ed952f0981a56464cc54c
jordyf15 <jordyferdian88@gmail.com>
2021-12-23 21:31:44 +0700

    styled profile layout

--- resources/views/layout/profileLayout.blade.php
+++ resources/views/layout/profileLayout.blade.php
@@ -1,15 +1,17 @@
 @extends('layout.layout')
 @section('content')
-<main>
-    <div id='side-tab'>
-        @auth
-            <a href="/profile">Profile</a>
-            @if (Auth::user()->role == 'member')
-                <a href="/friends">Friends</a>
-                <a href="/transactionHistory">Transaction History</a>
-            @endif   
-        @endauth
+<main id='profileLayout-main'>
+    <div id='profileLayout-container'>
+        <div id='side-tab'>
+            @auth
+                <a href="/profile">Profile</a>
+                @if (Auth::user()->role == 'member')
+                    <a href="/friends">Friends</a>
+                    <a href="/transactionHistory">Transaction History</a>
+                @endif   
+            @endauth
+        </div>
+        @yield('page')
     </div>
-    @yield('page')
 </main>
 @endsection
